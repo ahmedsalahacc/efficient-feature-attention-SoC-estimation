f32afc47295ce53180eada55a89e7e20cf162727
Ahmed Abdulmaksoud <ahmedsalaheldin125@gmail.com>
2025-03-23 06:04:54 -0400

    feat: add refactored test file and configs

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,38 @@
-FROM pytorch/pytorch:2.5.1-cuda12.4-cudnn9-runtime
+# Use NVIDIA CUDA base image with Python
+FROM nvidia/cuda:12.4.1-devel-ubuntu22.04
 
+# Set working directory
 WORKDIR /app
 
-COPY requirements.txt requirements.txt
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    python3-pip python3-dev && \
+    rm -rf /var/lib/apt/lists/*
 
-RUN pip install --no-cache-dir -r requirements.txt
+# Set Python3 as default
+RUN ln -s /usr/bin/python3 /usr/bin/python
 
-COPY /datasets /app/datasets
+COPY requirements.txt .
 
-COPY /feature_based_transformer.ipynb /app/src/feature_based_transformer.ipynb
+# Install Python dependencies
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+RUN pip install notebook
+# Copy project files (if needed)
+COPY . .
+
+# Set environment variables for CUDA
+ENV CUDA_HOME=/usr/local/cuda
+ENV PATH=/usr/local/cuda/bin:$PATH
+ENV LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+# Ensure NVIDIA runtime is used
+ENV NVIDIA_VISIBLE_DEVICES all
+ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
+
+EXPOSE 8888
+
+# Define entrypoint
+# CMD ["python", "src/main.py"]
+CMD ["jupyter-notebook","--allow-root"]
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,134 @@
-def count_parameters(model):
+"""Helper functions frequently in the project."""
+
+import os
+
+import glob
+
+import numpy as np
+
+import torch
+
+from typing import List, Protocol, Iterable, Union, Type
+
+from datasets.LGDataset import LGDataset, get_dataloader
+
+
+class TorchModel(Protocol):
+    def parameters(self) -> Iterable[float]:
+        """Returns the parameters of the torch model"""
+
+
+def count_parameters(model: TorchModel) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def fetch_data_files(path: str, mode="train") -> Union[List[str], str]:
+    """Fetches the csv files in the path.
+
+    Parameters
+    ----------
+    path: str
+        path to get the files from
+    mode: str
+        must be either train|valid|test
+
+    Returns
+    -------
+    List[str]
+        List of files
+    """
+    assert mode in ("train", "valid", "test"), "Mode must be one of train|valid|test."
+    path = os.path.join(path, mode, "*.csv")
+    files = glob.glob(os.path.abspath(path))
+
+    if mode in ("train", "valid"):
+        if files:
+            return files[0]
+        raise FileNotFoundError(f"{path} has no csv files.")
+    else:
+        if files:
+            return files
+        raise FileNotFoundError(f"{path} has no csv files.")
+
+
+def test_model_on_multiple_temps(model, batch_size, test_files: List[str]) -> None:
+    """
+    Evaluates the performance of a model on multiple temperature-based datasets.
+
+    This function takes a model and evaluates it on a list of temperature-specific datasets.
+    For each dataset, it calculates and prints the performance metrics (MSE, RMSE, MAE, MAXE).
+    After evaluating all files, it computes the overall performance across all datasets.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The model to be evaluated. It is assumed that the model has a `.cuda()` method for moving to GPU.
+
+    batch_size : int
+        The batch size used for loading the dataset during evaluation.
+
+    test_files : List[str]
+        A list of file paths, each representing a temperature-specific dataset in CSV format.
+        The model will be evaluated on each file sequentially.
+
+    Returns
+    -------
+    None
+        This function does not return any value. It prints evaluation results for each dataset and overall performance.
+
+    Notes
+    -----
+    The function only works with `LGDataset` class to load data from the provided file paths.
+    The performance metrics are printed in percentage terms.
+    """
+    # predefine the collective preds and labels placeholder
+    y_preds_all = []
+    y_trues_all = []
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model.to(device)
+
+    # evaluate each file
+    for file in test_files:
+        test_dataset = LGDataset(file)
+        test_loader = get_dataloader(test_dataset, batch_size=batch_size, shuffle=False)
+
+        y_preds = []
+        y_trues = []
+        # evaluate preds
+        for x, y in test_loader:
+            y_preds.append(model(x.to(device)).detach().cpu().numpy())
+            y_trues.append(y.numpy())
+
+        # concatenate all batches
+        y_preds = np.concatenate(y_preds)
+        y_trues = np.concatenate(y_trues)
+
+        y_preds_all += y_preds.tolist()
+        y_trues_all += y_trues.tolist()
+
+        # evaluate model performance
+        mse = np.mean((y_preds - y_trues) ** 2)
+        rmse = np.sqrt(mse)
+        mae = np.mean(np.abs(y_preds - y_trues))
+        maxe = np.max(np.abs(y_preds - y_trues))
+        print("File:", file)
+        print(
+            f"\t-MSE%: {mse*100:.3f}, RMSE%: {rmse*100:.3f}, MAE%: {mae*100:.3f}, MAXE%: {maxe*100:.3f}"
+        )
+        print()
+
+    # Evaluate overall performance
+    print("<Overall>")
+    # evaluate model performance
+    y_preds_all = np.array(y_preds_all)
+    y_trues_all = np.array(y_trues_all)
+
+    mse = np.mean((y_preds_all - y_trues_all) ** 2)
+    rmse = np.sqrt(mse)
+    mae = np.mean(np.abs(y_preds_all - y_trues_all))
+    maxe = np.max(np.abs(y_preds_all - y_trues_all))
+    print(
+        f"MSE%: {mse*100:.3f}, RMSE%: {rmse*100:.3f}, MAE%: {mae*100:.3f}, MAXE%: {maxe*100:.3f}"
+    )
